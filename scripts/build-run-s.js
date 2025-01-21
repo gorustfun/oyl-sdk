665b38b006dc0d4a9ad63b0ee55d7e652e1991f3
kungfuflex <judoflexchop@gmail.com>
2025-01-21 12:06:12 -0600

    update

--- scripts/build-run-s.js
+++ scripts/build-run-s.js
@@ -1,3 +1,5 @@
 'use strict';
-process.chdir(path.join(__dirname, 'build'));
+const path = require("node:path");
+const child_process = require("node:child_process");
+process.chdir(path.join(__dirname, '..', 'build'));
 child_process.spawnSync('webpack', [], { stdio: 'inherit' });
